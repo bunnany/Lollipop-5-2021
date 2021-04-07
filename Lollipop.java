fcb711a8d58b63690d7522cbff19c34f0afc6b9c
Bunna Ny <bunna.ny@onslow.school.nz>
2021-04-08 09:24:49 +1200

    Improved design added constants, variables, parameters

--- Lollipop.java
+++ Lollipop.java
@@ -15,28 +15,49 @@ import java.awt.Color;
  * @version 1/04/21
  */
 public class Lollipop {
-
+    /* Fields */
+    public static final double X = 300.0;       // horizontal center of lollipop
+    public static final double Y = 180.0;       // vertical center of lollipop
+    public static final double SIZE = 80.0;     // diameter of lollipop
+    public static final double STICK = 200.0;   // length of the lollipop stick
+    
     /**
      * Constructor for objects of class Lollipop 
      */
     public Lollipop() {
         UI.initialise();
-        //UI.addButton("Quit", UI::quit);         // Comment out to checkstyle
+        UI.addButton("Quit", UI::quit);         // Comment out to checkstyle
     }
 
     /**
      * Draw a red lollipop on a stick 
      *
      */
-    private void drawLollipop() {
+    private void drawLollipop(double x, double y, double size, double stick) {
+        double left = x-size/2.0;
+        double top = y-size/2.0;
+        double bottom = y+stick;
+        
         /* Drawing the stick */
-        UI.setLineWidth(10);                // set line width to 10
-        // draw a line (300, 200) to (300, 400)
-        UI.drawLine(300, 200, 300, 400);    
+        UI.setColor(Color.black);            // reset stick color
+        UI.setLineWidth(size/8.0);           // set line width to 10
+        UI.drawLine(x, y, x, bottom);    
+        
         /* Drawing the lolly */
         UI.setLineWidth(1);                 // set line width to 1
         UI.setColor(Color.red);             // set color to red
-        UI.fillOval(260, 160, 80, 80);      // fill oval @(260, 160), 80x80
+        UI.fillOval(left, top, size, size);      // fill oval @(260, 160), 80x80
+    }
+    
+    /**
+     * Calls the drawLollipop method with a number of different parameters
+     */
+    public void doDrawLollipop() {
+        /* ask user for diameter */
+        double diam = UI.askDouble("Diameter: ");
+        drawLollipop(300, 180, diam, 200);
+        drawLollipop(50, 60, diam/2.0, 90);
+        drawLollipop(400, 100, diam, 70);
     }
     
     /**
@@ -45,7 +66,7 @@ public class Lollipop {
      */
     public static void main(String[] args) {
         Lollipop obj = new Lollipop();
-        obj.drawLollipop();                  // call the lollipop method
+        obj.doDrawLollipop();                  // call the lollipop method
     }
 
 }
